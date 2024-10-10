0f9fa492f19ee19c4d1ade148020b518444c7b80
unknown <@DESKTOP-81SKH73>
2024-10-10 15:24:43 +0200

    Добавьте файлы проекта.

---
+++ lab1.2/Program.cs
@@ -0,0 +1,17 @@
+﻿try
+{
+    Console.WriteLine("Введите R1");
+    double r1 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите R2");
+    double r2 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите R3");
+    double r3 = double.Parse(Console.ReadLine());
+    double v1 = (4.0 / 3.0 ) * Math.PI * r1 * r1 * r1;
+    double v2 = (4.0 / 3.0 ) * Math.PI * r2 * r2 * r2;
+    double v3 = (4.0 / 3.0) * Math.PI * r3 * r3 * r3;
+    Console.WriteLine($"Z={(v1 + v2 + v3) / 3}");
+}
+catch
+{
+    Console.WriteLine("Введите корректные данные");
+}
